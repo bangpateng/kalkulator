0b2c0234f984e479894e3d834321563addb31db7
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2024-02-22 02:53:42 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,6 @@
+const { add, subtract, multiply, divide } = require('./calculator');
+
+console.log(add(5, 3));       // Output: 8
+console.log(subtract(7, 2));  // Output: 5
+console.log(multiply(4, 6));  // Output: 24
+console.log(divide(8, 2));    // Output: 4
